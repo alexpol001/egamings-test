785de7f0dd51fc9ade527bff26f58c5244b6d681
Alex Pol <sinbix001@gmail.com>
2020-10-02 16:47:27 +0300

    feat: add header

--- libs/feature-main/src/lib/egamings-main.module.ts
+++ libs/feature-main/src/lib/egamings-main.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { UiModule } from '@egamings/ui';
+
 import { EgamingsMainComponent } from './egamings-main.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, UiModule],
   declarations: [EgamingsMainComponent],
   exports: [EgamingsMainComponent],
 })
