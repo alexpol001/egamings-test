463b4c6c870731e0d4daaaf8a9b14483a89adcbd
Alex Pol <sinbix001@gmail.com>
2020-10-06 16:23:40 +0300

    feat: add custom-webpack config file

---
+++ webpack.config.ts
@@ -0,0 +1,10 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+
+export default (config: webpack.Configuration) => {
+  config.resolve.alias['@egamings/theming'] = path.resolve(
+    __dirname,
+    'libs/shared/scss/_theming'
+  );
+  return config;
+};
