d1215a509b9705979749b04863ed1dd46d7eea88
Alex Pol <sinbix001@gmail.com>
2020-10-06 16:18:34 +0300

    feat: header statistics

--- libs/feature-main/src/lib/egamings-main.component.ts
+++ libs/feature-main/src/lib/egamings-main.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { GamesQuery } from '@egamings/data-access';
+import { GamesParamsQuery, GamesQuery } from '@egamings/data-access';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'egamings-main',
@@ -7,6 +9,22 @@ import { GamesQuery } from '@egamings/data-access';
   styleUrls: ['./egamings-main.component.scss'],
 })
 export class EgamingsMainComponent implements OnInit {
+  total$: Observable<number>;
 
-  ngOnInit(): void {}
+  filtered$: Observable<number>;
+
+  constructor(
+    private gamesQuery: GamesQuery,
+    private gamesParamsQuery: GamesParamsQuery
+  ) {}
+
+  ngOnInit(): void {
+    this.total$ = this.gamesQuery
+      .selectAll()
+      .pipe(map((games) => games?.length));
+
+    this.filtered$ = this.gamesParamsQuery
+      .getParamedGames()
+      .pipe(map((games) => games?.length));
+  }
 }
