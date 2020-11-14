dc5ec57be932fc652e224edf0948f6614f10717f
Alex Pol <sinbix001@gmail.com>
2020-11-14 13:08:16 +0300

    refactor: common

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,7 +4,7 @@ import * as webpack from 'webpack';
 export default (config: webpack.Configuration) => {
   config.resolve.alias['@egamings/theming'] = path.resolve(
     __dirname,
-    'libs/shared/client/ng-ui/theming/_theming'
+    'libs/shared/client/ng-ui/_theming'
   );
   return config;
 };
