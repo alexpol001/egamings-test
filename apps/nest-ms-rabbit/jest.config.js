1ca48d4c2c26a98bba531d0692869631e0acc227
Alex Pol <sinbix001@gmail.com>
2020-11-14 20:11:57 +0300

    feat: common

--- apps/nest-ms-rabbit/jest.config.js
+++ apps/nest-ms-rabbit/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  name: 'nest-rabbit',
+  name: 'nest-ms-rabbit',
   preset: '../../jest.config.js',
   globals: {
     'ts-jest': {
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/nest-rabbit',
+  coverageDirectory: '../../coverage/apps/nest-ms-rabbit',
 };
